58996550634cadef1b33349a04e6af697e9a07ac
Alex Yang <himself65@outlook.com>
2023-09-18 18:01:21 -0500

    fix: add catch

--- electron/main.ts
+++ electron/main.ts
@@ -11,19 +11,19 @@ import path from 'node:path'
 // │ │ └── preload.js
 // │
 process.env.DIST = path.join(__dirname, '../dist')
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
-
+process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(
+  process.env.DIST, '../public')
 
 let win: BrowserWindow | null
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
-function createWindow() {
+function createWindow () {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   // Test active push message to Renderer-process.
@@ -32,10 +32,14 @@ function createWindow() {
   })
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    win.loadURL(VITE_DEV_SERVER_URL).catch((reason) => {
+      console.error('loadURL failed:', reason)
+    })
   } else {
     // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    win.loadFile(path.join(process.env.DIST, 'index.html')).catch((reason) => {
+      console.error('loadFile failed:', reason)
+    })
   }
 }
 
