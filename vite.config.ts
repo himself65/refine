9944e4ecf00913069a6bc3e7cef282d7d0b0c617
Alex Yang <himself65@outlook.com>
2023-10-12 20:29:37 -0500

    ci: add build ci (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
 import electron from 'vite-plugin-electron'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
