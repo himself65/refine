14df5dff59ffae5e5b05e0ee7c6d9161f8fd1c81
Alex Yang <himself65@outlook.com>
2023-10-13 22:01:09 -0500

    refactor: use pnpm (#4)

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  fullyParallel: true,
+  timeout: process.env.CI ? 50_000 : 30_000,
+  webServer: {
+    command: 'pnpm run dev',
+    port: 5173
+  },
+  reporter: process.env.CI
+    ? [
+      ['html', { open: 'never', outputFolder: 'artifacts/html-report' }],
+      ['github'],
+      ['line'],
+    ]
+    : 'list'
+}
+
+if (process.env.CI) {
+  config.retries = 3
+  config.workers = '50%'
+}
+
+export default config
