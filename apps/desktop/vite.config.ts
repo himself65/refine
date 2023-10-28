cbc8f3d7a9e9a0f31dcb2406b668df6f763231d9
Alex Yang <himself65@outlook.com>
2023-10-27 20:41:14 -0500

    refactor: separate desktop and web version

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -3,18 +3,10 @@ import electron from 'vite-plugin-electron'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    outDir: '../desktop/dist',
-  },
   plugins: [
     react(),
     electron({
-      vite: {
-        build: {
-          outDir: '../desktop/dist-electron'
-        }
-      },
-      entry: '../desktop/src/main.ts'
+      entry: './src/electron/main.ts'
     })
   ]
 })
---
+++ packages/core/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        app: './src/app.tsx'
+      },
+      formats: ['es', 'cjs']
+    },
+    outDir: './dist',
+    rollupOptions: {
+      external: [
+        /^react/,
+        /^react-dom/,
+        /^@blocksuite/,
+        /^@toeverything/,
+        /^jotai/,
+        /^yjs/,
+        /^jotai-effect/
+      ]
+    }
+  },
+  plugins: [
+    react(),
+    dts()
+  ]
+})
