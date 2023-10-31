d9c6e182aa53000714c987c4b6cfcc23ad55aa1b
Alex Yang <himself65@outlook.com>
2023-10-30 22:26:03 -0500

    fix: lint

--- packages/jotai-inject/src/index.ts
+++ packages/jotai-inject/src/index.ts
@@ -23,6 +23,7 @@ export function inject<
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const effectWeakMap = new WeakMap<PrimitiveAtom<any>, Atom<void>>()
 
 export function useInject<Value> (
