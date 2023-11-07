0aac0e5acb52332ba736542798f22a48e61ad58d
Alex Yang <himself65@outlook.com>
2023-11-07 09:59:59 -0600

    fix: docs building issue

--- apps/docs/components/preview.tsx
+++ apps/docs/components/preview.tsx
@@ -1,47 +1,46 @@
 import type { ReactElement } from 'react'
 import {
-  type FC,
-  type PropsWithChildren,
-  use,
-  useEffect,
-  useState
+  use
 } from 'react'
-import { themeAtom } from '@refine/core/store'
-import { inject } from 'jotai-inject'
+import { useIsClient } from 'foxact/use-is-client'
+import { workspaceManager } from '@refine/core/store'
+import { getDefaultStore } from 'jotai/vanilla'
 
-inject(themeAtom, () => 'light' as const, () => {
-  alert('Change theme is not supported in preview mode')
-})
-
-let importAppPromise: Promise<typeof import('@refine/core/app')>
+let importAppPromise: Promise<typeof import('@refine/core/components')>
+let promise: Promise<void>
+const store = getDefaultStore()
 if (typeof window !== 'undefined') {
-  importAppPromise = import('@refine/core/app')
-} else {
-  importAppPromise = Promise.resolve({
-    App: () => <></>
-  })
-}
-
-const NoSsr: FC<PropsWithChildren> = ({
-  children
-}) => {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-  return (
-    <>
-      {mounted ? children : null}
-    </>
-  )
+  importAppPromise = import('@refine/core/components')
+  promise = store.get(workspaceManager.getWorkspaceAtom('workspace:0')).
+    then(async workspace => {
+      const page = workspace.createPage('page0')
+      await page.waitForLoaded()
+      const pageBlockId = page.addBlock('affine:page', {
+        children: [],
+        title: new page.Text('Hello, World')
+      })
+      page.addBlock('affine:surface', {}, pageBlockId)
+      const noteBlockId = page.addBlock('affine:note', {}, pageBlockId)
+      page.addBlock('affine:paragraph', {}, noteBlockId)
+    })
 }
 
 export const Preview = (): ReactElement => {
+  const isClient = useIsClient()
+  if (!isClient) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
   const { Editor } = use(importAppPromise)
+  use(promise)
   return (
-    <NoSsr>
-      <App
-        className="w-auto h-96 overflow-scroll border-solid border-2 border-indigo-600"/>
-    </NoSsr>
+    <Editor
+      className="w-auto h-96 overflow-scroll border-solid border-2 border-indigo-600"
+      workspaceId="workspace:0"
+      pageId="page0"
+    />
   )
 }
